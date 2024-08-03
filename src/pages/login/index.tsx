0c6afa74cfa752e109ebda99ac0aae2d33eba0c3
KiritoEM <johankirito64@gmail.com>
2024-08-03 16:10:42 +0300

    features:add ressponsive to login form

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -10,11 +10,11 @@ const login = (): JSX.Element => {
   return (
     <Fragment>
       <Title title="Se connecter à votre compte" />
-      <section className="login flex">
-        <div className="login__form w-[50%] flex justify-center items-center">
-          <div className="login__form-container max-w-[420px] md:w-[410px] flex flex-col items-start gap-9 my-12">
+      <section className="login flex w-full min-h-screen items-center lg:items-stretch overflow-x-hidden">
+        <div className="login__form w-full min-h-screen h-full lg:w-[50%] flex justify-center items-center">
+          <div className="login__form-container px-4 lg:px-0 max-w-[420px] md:w-[400px] flex flex-col items-center lg:items-start gap-9 my-[5vh] lg:my-12">
             <Logo />
-            <h3 className="title text-4xl font-calSans">
+            <h3 className="title text-[31px] md:text-3xl lg:text-4xl font-calSans text-center lg:text-start">
               Se connecter à{" "}
               <span className="text-primary font-calSans">BioSync</span>
             </h3>
@@ -66,7 +66,7 @@ const login = (): JSX.Element => {
           </div>
         </div>
         <div
-          className="login_image w-[50%] min-h-screen bg-cover bg-center bg-no-repeat flex items-end justify-center p-7"
+          className="login_image w-[50%] min-h-screen bg-cover bg-center bg-no-repeat hidden lg:flex items-end justify-center p-7"
           style={{ backgroundImage: `url("/login-bg.svg")` }}
         >
           <p className="font-calSans text-white text-[26px] text-center leading-tight">
